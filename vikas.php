8024e3c9856f763ef58bbc87491cb3b65c977a29
vikas <vikas08207@gmail.com>
2015-07-14 13:24:47 +0530

    version2.1

--- vikas.php
+++ vikas.php
@@ -1,19 +1,19 @@
 <?php
 include_once("config.php");
 // fetch channel id from databse
-
-	$result = mysqli_query($con,'SELECT cid FROM youtubeChannelsList WHERE sno >=1 AND sno <=200');
+    define("Num_between","sno >=1 AND sno <=200");
+	$result = mysqli_query($con,'SELECT cid FROM youtubeChannelsList WHERE Num_between');
 	$result_array = array();
-		$i=0;
+		$count=0;
 	
 	 while($row = mysqli_fetch_assoc($result))  // stored channel id in an array(result_array);
 	{
     $result_array[$i] = $row['cid'];
-       $i++;
+       $count++;
 	} 
 	
 	// declare variables 
-	$y=1;
+	$y=0;
 	$z=0;
 	$v=1;
 	$ch=array();
@@ -22,12 +22,12 @@ include_once("config.php");
 	
 	// create multiple parallel connection using curl_multi_* function
 	
-while($y <= 200)										
+while($y <= $count)										
 {
 	$z = $y;
 	                                            //initialize curl and made 50 different channel
 				$mh = curl_multi_init();
-						for($j=1;$j<100;$j++)				
+						for($j=0;$j<100;$j++)				
 						{
 							$ch[$j] = curl_init();								//initialze curl
 							curl_setopt($ch[$j], CURLOPT_URL, "https://www.googleapis.com/youtube/v3/subscriptions?part=snippet,id,contentDetails&fields=nextPageToken,items(id,snippet(publishedAt,resourceId(channelId),channelId))&channelId=".$result_array[$z]."&maxResults=50&key=AIzaSyAi5EGrpxTLFuADJ2jqut5ftAsfKG-VPVI");
@@ -46,41 +46,42 @@ while($y <= 200)
 							 } while($running > 0);
 	
 							$x=0;
-							$k=1;
+							
 							$data=array();
-							       for($j=1;$j<100;$j++)                //get the content of all channel and stored in a data() array
+							       for($j=0;$j<100;$j++)                //get the content of all channel and stored in a data() array
 								   {
+									   $k=0;
 										$data[$j]=curl_multi_getcontent($ch[$j]);
 										$obj = json_decode($data[$j]);				//convert the json text into object and stored in an object
-											if(isset($obj))
-											{			
-									   $result= count($obj->items);                  //count total number of element (i.e. item)in json
-										$file = fopen("test7.txt","a+");			//create and open file in append mod
+										if(isset($obj->item))
+										{			
+									       $result= count($obj->items);                  //count total number of element (i.e. item)in json
+										   $file = fopen("test7.txt","a+");			//create and open file in append mod
 												while($result>0)
-													 {												// check whether any element exist in item or not
+												{												// check whether any element exist in item or not
 														if(isset($obj->items[$k]))		//convert from object to text form
-															{
+														{
 															fwrite($file, trim(json_encode($obj->items[$k]))."\n");  //write the value of item in file
 																 $k++;
-															}	
+														}	
 													$result--;	
-													}
-													fclose($file);								//close file
+												}
+												fclose($file);								//close file
 																										
-													$token=$obj->nextPageToken;													
-													if( $token )     //check whether any nextPageToken or not if yes then store in a array i.e. nextToken
-													{									 
-													 $nextToken[$x]= $obj->nextPageToken;
-													 $index[$x] = $j;
-													 $x++;
+												$token=$obj->nextPageToken;													
+												if( $token )     //check whether any nextPageToken or not if yes then store in a array i.e. nextToken
+												{									 
+												  $nextToken[$x]= $obj->nextPageToken;
+												  $index[$x] = $j;
+												  $x++;
 													//echo $result_array[$j]."<br>";		 
-													}		//end if condition	
+												}		//end if condition	
 														
-										}
+										 }
 										curl_multi_remove_handle($mh, $ch[$j]);     //remove all chanel handle
-								} //End of while loop
+								 } //End of while loop
 										
-								if($x > 0)     //check whether any nextPageToken or not if yes then store in a array called nextToken
+							  if($x > 0)     //check whether any nextPageToken or not if yes then store in a array called nextToken
 							  {			            			
 								 goto nextpage;
 							  }	
@@ -89,9 +90,9 @@ curl_multi_close($mh);
 $y = $z;			
 		// herewe do process again till if there  exist nextPageToken 							
 			nextpage:	
-				{			$p=1;				
+				{			$p=0;				
 						$mh = curl_multi_init();
-						for($j=1; $j<100 ;$j++)			
+						for($j=0; $j<100 ;$j++)			
 						{	
 							if($x>0)
 							{						
@@ -102,7 +103,7 @@ $y = $z;
 							curl_setopt($ch[$j], CURLOPT_HEADER, false);
 							
 							curl_multi_add_handle($mh, $ch[$j]);					//add all channel in multichannel
-							//echo $result_array[$n]."<br>";
+							echo $result_array[$n]."<br>";
 							$p++;
 							$x--;
 							}												// calculate total number of channel
@@ -116,47 +117,45 @@ $y = $z;
 								curl_multi_exec($mh, $running);
 							 } while($running > 0);	
 						 
-							$k=1;
-							      for($j=1;$j<=$p;$j++)                //get the content of all channel and stored in a data() array
-								   { 									   									   
+							
+							      for($j=0;$j<=$p;$j++)                //get the content of all channel and stored in a data() array
+								   { 
+									   $k=0;									   									   
 										$data[$j]=curl_multi_getcontent($ch[$j]);
 										$obj = json_decode($data[$j]);				//convert the json text into object and stored in an object
-											if(isset($obj))
-											{			
-									   $result= count($obj->items);                  //count total number of element (i.e. item)in json
-										$file = fopen("test7.txt","a+");			//create and open file in append mod
-												while($result>0)
-													 {												// check whether any element exist in item or not
-														if(isset($obj->items[$k]))		//convert from object to text form
-															{
-															fwrite($file, trim(json_encode($obj->items[$k]))."\n");  //write the value of item in file
-																 $k++;
-															}	
-													$result--;	
-													}
-													fclose($file);								//close file
-																									
-													$token=$obj->nextPageToken;	
-													echo $token;
-													 echo "vikas";												
-													if( $token )     //check whether any nextPageToken or not if yes then store in a array i.e. nextToken
-													{																						 
-													 $nextToken[$x]= $obj->nextPageToken;
-													 $index[$x] = $j;
-													 $x++;
-														
-													//echo $result_array[$j]."<br>";		 
-													}		//end if condition	 
-												}	
+										if(isset($obj))
+										{			
+											  $result= count($obj->items);                  //count total number of element (i.e. item)in json
+											  $file = fopen("test7.txt","a+");			//create and open file in append mod
+											  while($result>0)
+											  {												// check whether any element exist in item or not
+												if(isset($obj->items[$k]))		//convert from object to text form
+												{
+												 fwrite($file, trim(json_encode($obj->items[$k]))."\n");  //write the value of item in file
+												 $k++;
+												 }	
+												$result--;	
+											  }
+											  fclose($file);								//close file
+																										
+											  $token=$obj->nextPageToken;	
+											  if( $token )     //check whether any nextPageToken or not if yes then store in a array i.e. nextToken
+											  {																						 
+												$nextToken[$x]= $obj->nextPageToken;
+												$index[$x] = $j;
+											    $x++;
+										        echo $token."<br>";		 
+											   }		//end if condition	 
+										}	
 																						   
-										} 
+									} 
 								curl_multi_remove_handle($mh, $ch[$j]);  			 //remove all chanel handle
 	
-								if($x > 0)     //check whether any nextPageToken or not if yes then store in a array called nextToken
-							  {		
+							if($x > 0)     //check whether any nextPageToken or not if yes then store in a array called nextToken
+							{		
 								  //echo "<br>";	            			
 								 goto nextpage;
-							  }	
+							 }	
 						
 						$y = $z;	
 			}   
